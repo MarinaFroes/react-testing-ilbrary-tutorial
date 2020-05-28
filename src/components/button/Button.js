a0dac14936c196ffc6fa1c08982451d9b33368a9
MFAC <ninafroes@gmail.com>
2020-05-28 17:49:59 +0200

    feat: Remove unecessary code and logo and create a button component with style file

---
+++ src/components/button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './button.css'
+
+function Button({ label }) {
+  return (
+    <div className="button-style" data-testid="button">{label}</div>
+  )
+}
+
+export default Button
