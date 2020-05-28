23cc37b2835be3ab6d669eccca7419057545c8c8
MFAC <ninafroes@gmail.com>
2020-05-28 18:44:31 +0200

    feat: Add snapshot testing

--- src/components/button/__test__/button.test.js
+++ src/components/button/__test__/button.test.js
@@ -4,6 +4,7 @@ import Button from '../Button'
 
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import renderer from 'react-test-renderer'
 
 afterEach(cleanup);
 
@@ -22,3 +23,15 @@ it("renders button correctly", () => {
   expect(getByTestId('button')).toHaveTextContent("Save");
 });
 
+// Snapshot testing
+it("matches snapshot 1", () => {
+  // .create() creates a snapshot and .toJSON() convert it to a virtual DOM object 
+  const tree = renderer.create(<Button label="Save" />).toJSON();
+  expect(tree).toMatchSnapshot();
+  // creates a folder called snapshot, where there is a file called button.test.js.snap and compares the new snapshot with the original
+})
+
+it("matches snapshot 2", () => {
+  const tree = renderer.create(<Button label="Click me" />).toJSON();
+  expect(tree).toMatchSnapshot();
+})
